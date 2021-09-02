b16a54b0bdd880c58ab856baec06b1a8a94b3ed9
Alan Sun <mralansun@gmail.com>
2021-09-02 18:55:51 -0400

    Updated motto positioning

--- src/components/TeamLanding.jsx
+++ src/components/TeamLanding.jsx
@@ -8,7 +8,7 @@ export default function TeamLanding() {
       <div className="absolute flex items-center justify-center text-white text-center h-full w-full">
         <h1 className="italic font-medium" style={{ textShadow: '9px 7px #4C1D95', fontSize: 'calc(8em + 2vw)' }}>WUFO</h1>
       </div>
-      <div className="absolute text-white right-20 bottom-40">
+      <div className="absolute text-white right-12 bottom-32">
         <h2 className="italic font-semibold text-4xl ml-4" style={{ textShadow: '3px 3px #4C1D95' }}>The best that Williams has to offer.</h2>
       </div>
     </div>
