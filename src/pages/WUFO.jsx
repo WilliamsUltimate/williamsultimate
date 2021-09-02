9897734ba60625e5095064d80455a5d2e314ae89
Rito Tanaka <rt7@williams.edu>
2021-09-02 01:06:00 -0400

    create roster and player component

--- src/pages/WUFO.jsx
+++ src/pages/WUFO.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Navbar from '../components/Navbar';
 import { HashLink as Link } from 'react-router-hash-link';
+import Roster from '../components/Roster';
 
 export default function WUFO() {
   return (
@@ -92,11 +93,15 @@ export default function WUFO() {
       <br/>
       <div className="flex flex-row justify-between p-4 w-full text-gray-500 z-10">
       <Link 
-        to='/wufo#top'
+        smooth to='/wufo#top'
         className="bg-white p-2 shadow-drop text-gray-shadow"
       >
         Go back to top
       </Link>
+      </div>
+      <div id="roster">
+        <br/>
+        <Roster team="wufo"/>
       </div>
     </div>
   )
