3abd2f1f7162e53553e136828ca5a5b547a978bb
Rito Tanaka <rt7@williams.edu>
2021-09-02 13:42:28 -0400

    fix lawufa roster

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,8 +4,12 @@ export default function Player(props) {
 
   return (
     <div class="w-full rounded">
-        <img src={require(`../assets/${props.team}pics/roster/${props.name}.jpg`).default}
-             alt={`${props.name}`}/>
+        {props.team === "wufo" ? 
+          <img src={require(`../assets/wufopics/roster/${props.name}.jpg`).default}
+               alt={`${props.name}`}/> :
+          <img src={require(`../assets/lawufapics/roster/${props.name}.jpg`).default}
+               alt={`${props.name}`}/>
+        }
         <div class="capitalize text-gray-shadow text-center">
           {props.name}
         </div>
--- src/pages/LaWUFA.jsx
+++ src/pages/LaWUFA.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import TeamLanding from '../components/TeamLanding';
 import { HashLink as Link } from 'react-router-hash-link';
 import Roster from '../components/Roster';
 
 export default function LaWUFA() {
   return (
-    <div>
+    <div className="flex flex-col">
+    <div className="top-0" id="top">
       <Navbar team="lawufa"/>
-      <div id="roster">
-        <br/>
-        <Roster team="lawufa"/>
-      </div>
     </div>
+    <div>
+      <TeamLanding />
+    </div>
+    <div className="flex flex-row justify-between p-4 w-full text-gray-500 z-10">
+    <Link 
+      smooth to='/lawufa#top'
+      className="bg-white p-2 shadow-drop text-gray-shadow"
+    >
+      Go back to top
+    </Link>
+    </div>
+    <div id="roster">
+      <br/>
+      <Roster team="lawufa"/>
+    </div>
+  </div>
   )
 }
