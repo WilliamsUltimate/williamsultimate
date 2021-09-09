0952c11ddf6e24173db4655e09a6db61b8fab28c
Rito Tanaka <rt7@williams.edu>
2021-09-09 17:22:40 -0400

    minor about cleanup

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Timeline } from 'react-twitter-widgets';
+// import { Timeline } from 'react-twitter-widgets';
 
 export default function Twitter(props) {
 
   return (
     <div>
-      <Timeline
+      {/* <Timeline
         dataSource={{ sourceType: "profile", screenName: props.handle }}
         options={{ width: "500", height: "600" }}
-      />
+      /> */}
     </div>
   )
 }
--- src/pages/WUFO.jsx
+++ src/pages/WUFO.jsx
@@ -5,7 +5,6 @@ import About from '../components/About';
 import Events from '../components/Events';
 import Twitter from '../components/Twitter';
 import Roster from '../components/Roster';
-import About from '../components/About';
 
 export default function WUFO() {
   return (
@@ -22,7 +21,7 @@ export default function WUFO() {
 
       {/* About */}
       <div id="about">
-        <About/>
+        <About team="wufo"/>
       </div>
 
       {/* News (Upcoming Events and Twitter) */}
