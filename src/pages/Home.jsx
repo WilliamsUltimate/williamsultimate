fa859c90eb16bbc7d85c0892e1633d52dc90c89e
Rito Tanaka <rt7@williams.edu>
2022-06-01 23:43:31 -0400

    full screen images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,47 +2,49 @@ import '../styles/home.css';
 import { Link } from 'react-router-dom';
 import wufo from '../assets/wufopics/wufo.png';
 import nova from '../assets/novapics/nova.png';
-import buf from '../assets/bufpics/buf.png';
-import charlie from '../assets/wufopics/charlie-sky.jpg';
-import annika from '../assets/novapics/annika-sky.JPG';
-import dog from '../assets/bufpics/dog.jpg';
+import charlie from '../assets/wufopics/charlie-square.jpg';
+import annika from '../assets/novapics/annika-square.JPG';
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen justify-around">
+    <div className="flex flex-col max-h-screen">
 
-      {/* WUF text logo */}
-      <div className="flex justify-evenly text-wurple font-extrabold font-serif" style={{fontSize: 'calc(0.5em + 2.5vw)'}}>
-          WILLIAMS ULTIMATE FRISBEE
+      {/* Title */}
+      <div className="flex justify-center text-2xl sm:text-4xl md:text-5xl text-wurple font-bold font-serif m-5 md:m-10">
+        WILLIAMS ULTIMATE
       </div>
 
-      {/* WUFO, NOVA, and BUF logo-buttons */}
-      <div className="flex flex-col md:flex-row justify-center space-y-8 md:space-y-0 md:space-x-4">
-        <div className="md:relative self-center flex-initial md:w-64">
-          <Link to='/wufo' className="group">
-            <img src={charlie} alt="Charlie (#8) skying an opponent" className="hidden md:flex group-hover:opacity-60 transition-opacity"/>
-            <img src={wufo} alt="WUFO" className="md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 w-24"/>
-          </Link>
-        </div>
-        <div className="md:relative self-center flex-initial md:w-64">
-          <Link to='/nova' className="group">
-            <img src={annika} alt="Annika (#0) skying an opponent" className="hidden md:flex group-hover:opacity-60 transition-opacity"/>
-            <img src={nova} alt="NOVA" className="md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 w-24"/>
-          </Link>
-        </div>
-        <div className="md:relative self-center flex-initial md:w-64">
-          <Link to='/buf' className="group">
-            <img src={dog} alt="Dog with a frisbee in its mouth" className="hidden md:flex group-hover:opacity-60 transition-opacity"/>
-            <img src={buf} alt="BUF" className="md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 w-24"/>
-          </Link>
-        </div>
-      </div>
+      <div className="flex flex-col md:flex-row overflow-hidden">
 
-      {/* Footer */}
-      <div className="flex justify-evenly font-mono" style={{fontSize: '0.8em'}}>
-        Made with 💜 by Alan and Rito
-      </div>
+        {/* WUFO */}
+        <div className="relative md:w-1/2 overflow-hidden group">
+          <Link to='/wufo'>
+            <img  src={charlie}
+                  alt="WUFO background image"
+                  className="w-screen filter brightness-60 opacity-80 group-hover:opacity-60 transition-opacity"
+            />
+            <img  src={wufo}
+                  alt="WUFO logo"
+                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-24 md:w-36"
+            />
+          </Link>
+        </div>
 
+        {/* NOVA */}
+        <div className="relative md:w-1/2 overflow-hidden group">
+          <Link to='/nova'>
+            <img  src={annika}
+                  alt="NOVA background image"
+                  className="w-screen filter brightness-60 opacity-80 group-hover:opacity-60 transition-opacity"
+            />
+            <img  src={nova}
+                  alt="NOVA logo"
+                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-24 md:w-36"
+            />
+          </Link>
+        </div>
+
+      </div>
     </div>
   )
 }
